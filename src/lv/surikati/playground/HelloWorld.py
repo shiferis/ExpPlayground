9d8e3b53569ea0bf462a49e7d30653797a0a18d8
Narbagge <ingus.pret@gmail.com>
2015-07-12 13:30:34 +0100

    Added proper spacing.

--- src/lv/surikati/playground/HelloWorld.py
+++ src/lv/surikati/playground/HelloWorld.py
@@ -12,9 +12,9 @@ class HelloWorld(cocos.layer.Layer):
         super(HelloWorld, self).__init__()
 
         label = cocos.text.Label('Hello, world! :)',
-                          font_name='Times New Roman',
-                          font_size=32,
-                          anchor_x='center', anchor_y='center')
+                          font_name = 'Times New Roman',
+                          font_size = 32,
+                          anchor_x = 'center', anchor_y='center')
         label.position = 320,240
         self.add(label)
 
